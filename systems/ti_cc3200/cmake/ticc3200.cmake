ed80231a7e41e4cee71c322cb0eb7776913393b0
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2017-09-10 00:06:57 +0200

    Fixed CC3200 log

--- systems/ti_cc3200/cmake/ticc3200.cmake
+++ systems/ti_cc3200/cmake/ticc3200.cmake
@@ -2,6 +2,7 @@
 add_definitions(-Dgcc)
 add_definitions(-DSL_PLATFORM_MULTI_THREADED)
 add_definitions(-DUSE_FREERTOS)
+add_definitions(-DUSE_CC3200)
 add_definitions(-DxTARGET_IS_CC3200)
 
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
@@ -12,10 +13,6 @@ set(OBJCOPY arm-none-eabi-objcopy CACHE STRING "arm-none-eabi-objcopy" FORCE)
 set(OBJDUMP arm-none-eabi-objdump CACHE STRING "arm-none-eabi-objdump" FORCE)
 
 # Set compiler arguments
-SET(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -ffunction-sections -fdata-sections -MD -std=c11 -O0 -g --specs=nosys.specs")
-
-#set(CMAKE_C_LINK_EXECUTABLE
-#    "<CMAKE_C_COMPILER> -Wl,--entry=ResetISR,--gc-sections -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
-
+set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -ffunction-sections -fdata-sections -MD -std=c11 -O0 -g --specs=nosys.specs")
 
 set(CMAKE_EXE_LINKER_FLAGS "-T systems/ti_cc3200/linker.ld -Xlinker --gc-sections")
